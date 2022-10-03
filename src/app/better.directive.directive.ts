f9888918f6063dac016849655941f781958357ca
Hairsh Tagadghar <harishtgtg@gmail.com>
2022-10-03 12:34:41 +0530

    some modification using hostlistener & hostbinding

--- src/app/better.directive.directive.ts
+++ src/app/better.directive.directive.ts
@@ -1,10 +1,11 @@
-import { Directive , ElementRef ,Renderer2 , OnInit} from '@angular/core';
+import { Directive , ElementRef ,Renderer2 , OnInit , HostListener , HostBinding} from '@angular/core';
 
 @Directive({
   selector: '[appBetterDirective]'
 })
 export class BetterDirectiveDirective implements OnInit {
 
+  @HostBinding('style.backgroundColor') backgroundColor : string = 'green'
   constructor(private elRef: ElementRef , private renderar : Renderer2) {
     this.renderar.setStyle(this.elRef.nativeElement , 'color' , 'white')
 
@@ -12,5 +13,13 @@ export class BetterDirectiveDirective implements OnInit {
 
   ngOnInit(): void {
     this.renderar.setStyle(this.elRef.nativeElement , 'backgroundColor' , 'green')
+  }
+  @HostListener('mouseenter') mouseEnter(eventData : Event){
+    this.backgroundColor = 'red'
+    this.renderar.setStyle(this.elRef.nativeElement , 'color' , 'white')
+  }
+  @HostListener('mouseleave') mouseLeave(data : Event){
+    this.backgroundColor = 'transparent'
+    this.elRef.nativeElement.style.color = 'black'
   }
 }
